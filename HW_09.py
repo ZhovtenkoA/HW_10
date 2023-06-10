766866aeda62b75e6bce43b2b85fad8c7c7d04d7
ZhovtenkoA <127408550+ZhovtenkoA@users.noreply.github.com>
2023-06-10 23:50:56 +0300

    Update HW_09.py

--- HW_09.py
+++ HW_09.py
@@ -1,3 +1,56 @@
+from collections import UserDict
+
+class AddresBook(UserDict):
+    def __init__(self):
+        super().__init__()
+        
+    def add_record(self):
+        self.data[Name.name] = Phone.phone 
+        
+        
+class Field:
+    pass
+
+class Name(Field):
+    name = ''
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+class Phone(Field):
+    phone = ''
+    def __init__(self, phone):
+        super().__init__()
+        self.phone = [phone]
+
+
+class Record(AddresBook):
+    def __init__(self, command):
+        super().__init__()
+        self.command = command
+        command_l = command.split()
+        self.name = command_l[1]
+        self.phone = command_l[2]
+        self.name = Name(self.name)
+        self.phone = Phone(self.phone)
+        
+        
+
+    def add_phone(self):
+        #self.data[self.name] = self.phone
+        PHONES[self.name] = self.phone
+
+    def change_phone(self):
+        PHONES[self.name] = self.phone
+        #self.data[self.name] = self.phone
+    def delete_phone(self):
+        for phones in PHONES[self.name]:
+            if phones:
+                phones = 'empty'
+
+
+
+
 def input_error(func):
     def inner(*args):
         try:
@@ -17,11 +70,18 @@ def main():
     while running:
         command = input('Enter a command:  ').lower()
         if command.startswith('add'):
-             add_phones(command)
+            record = Record(command)
+            record.add_record()
+            #add_phones(command)
         elif command.startswith('change'):
-             change_phones(command)
+            record = Record(command)
+            record.change_phone()
+             #change_phones(command)
         elif command.startswith('phone'):
-             print_phones(command)
+            print_phones(command)
+        elif command.startswith('del'):
+            record = Record(command)
+            record.delete_phone()
         else:
             print(get_handler(command))
             if get_handler(command) == 'Good bye!':
@@ -43,18 +103,18 @@ def say_goodbye():
     return output
 
 
-@input_error
-def add_phones(command):
-    global PHONES
-    command_l = command.split()
-    PHONES[command_l[1]] = command_l[2]
+#@input_error
+#def add_phones(command):
+    #global PHONES
+    #command_l = command.split()
+    #PHONES[command_l[1]] = command_l[2]
     
 
-@input_error
-def change_phones(command):
-    global PHONES
-    command_c = command.split()
-    PHONES[command_c[1]] = command_c[2]
+#@input_error
+#def change_phones(command):
+    #global PHONES
+    #command_c = command.split()
+    #PHONES[command_c[1]] = command_c[2]
     
 
 @input_error
@@ -68,7 +128,7 @@ def all_phones():
     return PHONES
 
 
-PHONES = {}
+PHONES = AddresBook()
 COMMANDS = {
     'hello': say_hello(),
     'good bye': say_goodbye(),
